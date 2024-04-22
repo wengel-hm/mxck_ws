3ebb28756673815c33f16b65e373f91d554eb1db
william-engel <william.engel@web.de>
2024-04-22 19:57:58 +0200

    automate download using shell script

---
+++ src/lane_detection/bagfiles/download_bagfile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# download from https://drive.google.com/file/d/1KlgJWr1ZPpqOXtWjZxWnekJk77gWo5TT/view?usp=share_link
+wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1KlgJWr1ZPpqOXtWjZxWnekJk77gWo5TT' -O left_round.bag
