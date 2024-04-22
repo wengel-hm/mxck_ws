e2f0dd422faae6d02b84495b177df1da125035a2
william-engel <william.engel@web.de>
2024-04-22 23:00:29 +0200

    add download link to jpeg compressed bagfile

--- src/lane_detection/bagfiles/download_bagfile.sh
+++ src/lane_detection/bagfiles/download_bagfile.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # download from https://drive.google.com/file/d/1KlgJWr1ZPpqOXtWjZxWnekJk77gWo5TT/view?usp=share_link
 gdown 'https://drive.google.com/uc?id=1KlgJWr1ZPpqOXtWjZxWnekJk77gWo5TT' -O left_round.bag
+
+# JPEG
+# download from https://drive.google.com/file/d/1Xu_G1AHHHbsVGI-qmws_W9tYGRSyMsD4/view?usp=drive_link
+gdown 'https://drive.google.com/uc?id=1Xu_G1AHHHbsVGI-qmws_W9tYGRSyMsD4' -O left_round_jpeg.bag
+
